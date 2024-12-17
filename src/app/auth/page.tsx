aa7dbc94db096b6a12e6be61cb64733574dbcde6
Cabral <thicabral2018@outlook.com>
2024-12-16 22:22:44 -0300

    first commit

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -5,7 +5,7 @@ import { Divider } from '@mui/material';
 
 export default function register() {
   return (
-    <main className="flex lg:justify-center">
+    <main className="flex md:justify-center">
       <div className="flex flex-col gap-8 md:max-w-[450px] w-full">
         <WelcomeSectionHeader href="/" title="Prazer ter você no ConectaBem!" />
         <div className="flex flex-col gap-12 w-full">
